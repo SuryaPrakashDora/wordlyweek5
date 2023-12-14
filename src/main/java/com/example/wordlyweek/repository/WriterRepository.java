b5f314fcaabf9126ef1173e700ed14d38ec3b8ad
suryaprakashdora <prakashmadakam21@gmail.com>
2023-12-14 08:51:13 +0000

    solution

---
+++ src/main/java/com/example/wordlyweek/repository/WriterRepository.java
@@ -0,0 +1,29 @@
+/*
+ * You can use the following import statements
+ *
+ * import java.util.ArrayList;
+ * import java.util.List;
+ * 
+ */
+
+// Write your code here
+package com.example.wordlyweek.repository;
+
+import com.example.wordlyweek.model.Writer;
+import com.example.wordlyweek.model.Magazine;
+import java.util.ArrayList;
+import java.util.List;
+
+public interface WriterRepository {
+    ArrayList<Writer> getWriters();
+
+    Writer getWriterById(int writerId);
+
+    Writer addWriter(Writer writer);
+
+    Writer updatWriter(int writerId, Writer writer);
+
+    void deleteWriter(int writerId);
+
+    List<Magazine> getWriterMagazines(int writerId);
+}
